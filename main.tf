9b202e601d171bc4d03f22c53321f0853eac05ab
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 11:48:17 -0400

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,26 +11,22 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
-}
-
-data "aws_vpc" "default" {
-  default = true
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -38,9 +34,9 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.2.0"
 
-  name     = "blog"
-  min_size = 1
-  max_size = 2
+  name     = "${var.environment.name}-blog"
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
@@ -54,13 +50,13 @@ module "blog_autoscaling" {
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type   
+  instance_type = var.instance_type
 }
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name            = "blog-alb"
+  name            = "${var.environment.name}-blog-alb"
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
@@ -105,7 +101,7 @@ module "blog_alb" {
 
   target_groups = {
     ex-instance = {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
@@ -115,14 +111,14 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
 
-  name        =  "blog"
+  name        =  "${var.environment.name}-blog"
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = module.blog_vpc.vpc_id
 
