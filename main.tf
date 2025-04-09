3e76e9c3a316df959de9b8dac8d848514c3cc61a
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 10:32:03 -0400

    blah

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.blog_autoscaling.id
+      #target_id        = module.blog_autoscaling.id
       
       create_attachment = false
     }
