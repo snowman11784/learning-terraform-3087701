c59c7a06b50028aa430e867eaa458cc39b3caef3
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 11:56:15 -0400

    fixed vpc_id

--- main.tf
+++ main.tf
@@ -133,5 +133,5 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 }
