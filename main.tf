e69d32cf50238884688ea69469b3e96340e6b4c6
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 11:02:32 -0400

    added enable_deletion_protection and set to false

--- main.tf
+++ main.tf
@@ -64,7 +64,9 @@ module "blog_alb" {
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id] 
+  security_groups = [module.blog_sg.security_group_id]
+
+  enable_delete_protection = false 
 
   # Security Group
   security_group_ingress_rules = {
