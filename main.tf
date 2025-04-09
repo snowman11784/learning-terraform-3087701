bd4b2d47686f10429cf8d1dc75c52bbe91b135fa
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 02:25:42 -0400

    added back target_group_arns and removed target_id

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -99,7 +100,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
