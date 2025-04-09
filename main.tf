57d2b219f8c8058868bc384d4e5988b906a68529
Andrew Little <snowman11784@users.noreply.github.com>
2025-04-09 03:01:03 -0400

    image_id to target_id

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.blog_autoscaling.id
+      target_id        = module.blog_autoscaling.image_id
     }
   }
 
